dce172fbc693469b3c94ee5774e7de0d0bdbf5ed
dong-tingtsai <b20172017@gmail.com>
2024-07-18 08:28:23 +0800

    Initial commit

--- js/addcart.js
+++ js/addcart.js
@@ -1,189 +1,75 @@
-let currentItemPrice;
-let currentQuantity = 1;
-let cartCount = 0;
-let totalPrice = 0;
-// let productCards = document.querySelectorAll(".lightbox_cart");
-
-function openModal() {
-  // currentItemPrice = price;
-  // document.getElementsByClassName("product_money").innerHTML = "Price: $" + currentItemPrice;
-  // let currentQuantity = document.getElementsByClassName("qty").value;
-  document.getElementById("lightbox_cart").style.display = "block";
-  // productCards.forEach((productCard) => {
-  //     productCard.style.display = "block";
-  // })
- 
-}
-//light box selected 
-// let selectedItems = document.querySelectorAll('.product-pic')
-
-// selectedItems.forEach((selectedItem,i) => {
-//   selectedItem.addEventListener('click',() => {
-//     productCards[i].style.display = "block";
-//   })
-// })
-
-function closeModal(){
-  document.getElementById("lightbox_cart").style.display = "none";
-//   productCards.forEach((productCard) => {
-//     productCard.style.display = "none";
-// })
-}
-
-let count = document.querySelector('.qty')
-function increaseQuantity() {
-  currentQuantity++;
-  // console.log(currentQuantity)
-  count.value = currentQuantity;
-}
-
-function decreaseQuantity() {
-  if (currentQuantity > 1) {
-    currentQuantity--;
-    count.value = currentQuantity;
-  }
-}
-
-
-function updatePrice() {
-    var size = document.getElementById("sizeSelect").value;
-    var price
-    // 根據選擇的尺寸更新價格
-    switch (size) {
-        case "M":
-            price = 60;
-            break;
-        case "L":
-            price = 70;
-            break;
+Vue.createApp({
+  data() {
+    return {
+      products: [],
+      showLightbox: false,
+      selectedProduct: null,
+      checkBar: false,
+      cartlist: false,
+      cartItems: []
+    };
+  },
+  computed: {
+    cartCart() {
+      return this.cartItems.reduce((total, item) => total + item.count, 0);
+    },
+    totalPrice() {
+      return this.cartItems.reduce((total, item) => total + item.price * item.count, 0);
     }
-
-    // 將價格顯示在網頁上
-    document.getElementById("priceDisplay").innerText =  "$" + price;
-}
-
-
-function addToCart() {
-   let qty = parseInt( count.value);
-   document.querySelector(".check-cart-bar").style.display = "block";
-  //  const subtotal = currentItemPrice * qty;
-  //  const itemHTML = "<li>Price: $" + currentItemPrice + ", Quantity: <span class='highlight'>" + qty + "</span>, Subtotal: $" + subtotal.toFixed(2) + " <img src='trash.png' onclick='removeItem(this)'></li>";
- //   const itemHTML = "<li>Price: $" + currentItemPrice + ", Quantity: <span class='highlight'>" + qty + "</span>, Subtotal: $" + subtotal.toFixed(2) + " <img src='trash.png' onclick='removeItem(this)'></li>";
-  //  document.getElementById("cart-items").innerHTML += itemHTML;
-  //  updateTotal(subtotal);
-  closeModal();
-  updateCartCount(qty);
-}
-
-function updateCartCount(quantity) {
-  cartCount += quantity;
-  document.getElementById("cart-count").innerText = cartCount;
-}
-
-// function updateTotal(subtotal) {
-//   totalPrice += subtotal;
-//   document.getElementById("total").innerHTML = "Total: $" + totalPrice.toFixed(2);
-// }
-
-function openCart() {
-  document.getElementById("lightbox-cartlist").style.display = "block";
-}
-
-function closeCart() {
-  document.getElementById("lightbox-cartlist").style.display = "none";
-}
-
-// function removeItem(item) {
-//   const li = item.parentNode;
-//   const subtotal = parseFloat(li.innerText.split("$")[3]);
-//   li.parentNode.removeChild(li);
-//   updateTotal(-subtotal);
-//   const quantity = parseInt(li.innerText.split(": ")[1]);
-//   updateCartCount(-quantity);
-// }
-
-
-
-// function checkout() {
-//   alert("Total: $" + totalPrice.toFixed(2) + "\nThank you for your order!");
-
-//   document.getElementById("cart-items").innerHTML = "";
-//   totalPrice = 0;
-//   document.getElementById("total").innerHTML = "Total: $" + totalPrice.toFixed(2);
-//   cartCount = 0;
-//   document.getElementById("cart-count").innerText = cartCount;
-// }
-
-
-
-// function showLightBox_cart(){ 
-//     let lightBox_cart = document.getElementById("lightbox_cart");
-//     lightBox_cart.style.display = "block";
-      
-//   }
-  
-//   function cancelcart(){
-//     let lightBox_cart = document.getElementById("lightbox_cart");
-//     lightBox_cart.style.display = "none";
-//   }
-  
-//   function init_cart(){
-
-//     document.getElementById("cart-pic").onclick = showLightBox_cart;
-  
-//   }; 
-  
-//   window.addEventListener("load", init_cart, false);
-
-
-//   window.addEventListener("load", function(){
-//     let btnMinus = document.getElementsByClassName("fa-minus");
-// 	let qty = document.getElementsByClassName("qty");
-// 	let btnPlus = document.getElementsByClassName("fa-plus");
-
-// 	for(let i=0; i<btnMinus.length; i++){
-// 		btnMinus[i].onclick = function(){
-// 	     	if(parseInt(qty[i].value)>=1){
-// 				qty[i].value = parseInt(qty[i].value)-1;
-// 		      }
-// 			}
-// 	}
-
-// 	for(let i=0; i<btnPlus.length; i++){
-// 		btnPlus[i].onclick = function(){
-// 			qty[i].value = parseInt(qty[i].value)+1;
-// 		}
-// 	}
-
-
-// })
-
-
-// function updatePrice() {
-//     var size = document.getElementById("sizeSelect").value;
-//     var price;
-
-//     // 根據選擇的尺寸更新價格
-//     switch (size) {
-//         case "M":
-//             price = 60;
-//             break;
-//         case "L":
-//             price = 70;
-//             break;
-//     }
-
-//     // 將價格顯示在網頁上
-//     document.getElementById("priceDisplay").innerText =  "$" + price;
-// }
-
-
-// function addToCart() {
-//     const quantity = parseInt(document.getElementsByClassName("qty").value);
-//     const subtotal = currentItemPrice * quantity;
-//     const itemHTML = "<li>Price: $" + currentItemPrice + ", Quantity: <span class='highlight'>" + quantity + "</span>, Subtotal: $" + subtotal.toFixed(2) + " <img src='trash.png' onclick='removeItem(this)'></li>";
-//     document.getElementById("cart-items").innerHTML += itemHTML;
-//     updateTotal(subtotal);
-//     closeModal();
-//     updateCartCount(quantity);
-//   }
+  },
+  methods: {
+    subPrice(index) {
+     const item = this.cartItems[index];
+     return item.price * item.count;
+    },
+    openCartlist() {
+      this.cartlist = true;
+     },
+     closeCartlist() {
+      this.cartlist = false;
+     },
+    addCart(product) {
+      const existingItem = this.cartItems.find(item => item.id === product.id);
+      if(existingItem) {
+        // 如果商品已存在，增加數量
+        existingItem.count += product.count;
+      } else {
+        // 如果商品不存在，添加新商品到購物車
+        this.cartItems.push({...product});
+      }
+      this.checkBar= true;
+      this.showLightbox = false;
+    },
+   reduce(product) {
+    if(product.count > 1) {
+      product.count--;
+    }
+   },
+   add(product) {
+     product.count++;
+   },
+  removeItem(product) {
+    if(!product) return
+    const index = this.cartItems.findIndex(item => item.id === product.id);
+     // 移除陣列第幾個位置,一個
+    this.cartItems.splice(index, 1);
+  },
+   openLightbox(product) {
+    this.selectedProduct = product;
+    this.showLightbox = true;
+   },
+   closeLightbox() {
+    this.showLightbox = false;
+   },
+    fetchData() {
+      fetch("./assets/product.json")
+        .then((response) => response.json())
+        .then((data) => {
+          this.products = data;
+        });
+    },
+  },
+  mounted() {
+    this.fetchData();
+  },
+}).mount("#app");
--- js/store-locations.js
+++ js/store-locations.js
@@ -1,113 +1,35 @@
-// 全部地區
-document.querySelector('.active').addEventListener('click',function(){
+Vue.createApp({
+  data() {
+    return {
+      stores: [],
+      currentRegion: "all"
+    };
+  },
+  computed: {
+    filteredStores() {
+        if(this.currentRegion === "all") {
+            return this.stores;
+        }
 
-    let store = document.querySelectorAll('.store-card');
-    for(let i = 0 ; i < store.length ; i++){
-        store[i].style.display = 'block';
+        return this.stores.filter((item) => item.area === this.currentRegion);
     }
-    
-  })
+},
 
+  methods: {
+    setRegion(area) {
+        this.currentRegion = area;
+        this.activeTab = area;
+      },
 
-
-  // 北部篩選
-document.querySelector('.north-tab').addEventListener('click',function(){
-
-    document.querySelector('.central').style.display = 'none';
-
-    south = document.querySelectorAll('.south');
-    for(let i = 0 ; i < south.length ; i++){
-        south[i].style.display = 'none';
-    }
-
-    document.querySelector('.east').style.display = 'none';
-
-    north = document.querySelectorAll('.north');
-    for(let i = 0 ; i < north.length ; i++){
-        north[i].style.display = 'block';
-    }
-
-})
-
-
-  // 中部篩選
-  document.querySelector('.central-tab').addEventListener('click',function(){
-
-    north = document.querySelectorAll('.north');
-    for(let i = 0 ; i < north.length ; i++){
-        north[i].style.display = 'none';
-    }
-
-    south = document.querySelectorAll('.south');
-    for(let i = 0 ; i < south.length ; i++){
-        south[i].style.display = 'none';
-    }
-
-    let east = document.querySelector('.east');
-    east.style.display = 'none';
-
-    let central = document.querySelector('.central');
-    central.style.display = 'block';
-
-})
-
-
-  // 南部篩選
-  document.querySelector('.south-tab').addEventListener('click',function(){
-
-    north = document.querySelectorAll('.north');
-    for(let i = 0 ; i < north.length ; i++){
-        north[i].style.display = 'none';
-    }
-
-    let central = document.querySelector('.central');
-    central.style.display = 'none';
-
-    let east = document.querySelector('.east');
-    east.style.display = 'none';
-
-    south = document.querySelectorAll('.south');
-    for(let i = 0 ; i < south.length ; i++){
-        south[i].style.display = 'block';
-    }
-
-})
-
-
-  // 東部篩選
-  document.querySelector('.east-tab').addEventListener('click',function(){
-
-    north = document.querySelectorAll('.north');
-    for(let i = 0 ; i < south.length ; i++){
-        north[i].style.display = 'none';
-    }
-
-    let central = document.querySelector('.central');
-    central.style.display = 'none';
-
-    south = document.querySelectorAll('.south');
-    for(let i = 0 ; i < south.length ; i++){
-        south[i].style.display = 'none';
-    }
-
-    let east = document.querySelector('.east');
-    east.style.display = 'block';
-
-})
-
-
-var buttons = document.querySelectorAll('.store-tab');
-
-buttons.forEach(function(button, index) {
-    button.addEventListener('click', function() {
-        buttons.forEach(function(btn) {
-            btn.style.backgroundColor = 'white';
-            btn.style.color = '#714B23';
+    fetchData() {
+      fetch("./assets/store-locations.json")
+        .then((response) => response.json())
+        .then((data) => {
+          this.stores = data;
         });
-        button.style.backgroundColor = '#714B23';
-        button.style.color = 'white';
-    });
-});
-
-
-
+    }
+  },
+  mounted() {
+    this.fetchData();
+  },
+}).mount("#app");
